9cd67adad32af8ab6874f5d012a8d7ded2b2f108
Hailie17 <192151452@qq.com>
2024-02-26 22:55:02 +0800

    初始化项目

---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login-page">登录页</div>
+</template>
+
+<script>
+export default {
+  name: 'login-page'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login-page">注册页</div>
+</template>
+
+<script>
+export default {
+  name: 'login-page'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
