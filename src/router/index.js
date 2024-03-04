4748449502d781883ae3dd6e5c5f74033abc97a2
Hailie17 <192151452@qq.com>
2024-03-05 01:17:29 +0800

    项目路由搭建

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+import Detail from '@/views/Detail.vue'
+import Home from '@/views/Home.vue'
+import Article from '@/views/Layout/Article.vue'
+import Collect from '@/views/Layout/Collect.vue'
+import Like from '@/views/Layout/Like.vue'
+import User from '@/views/Layout/User.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  { path: '/login', component: Login },
+  { path: '/register', component: Register },
+  { path: '/detail', component: Detail },
+  {
+    path: '/',
+    redirect: '/article',
+    component: Home,
+    children: [
+      { path: 'article', component: Article },
+      { path: 'collect', component: Collect },
+      { path: 'like', component: Like },
+      { path: 'user', component: User }
+    ]
+  }
+]
 
 const router = new VueRouter({
   routes
