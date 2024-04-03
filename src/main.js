1547d8434dae92892bb480572b48d219b40338e3
Hailie17 <192151452@qq.com>
2024-04-04 00:31:10 +0800

    全局注册组件Vue.component('ArticleItem', ArticleItem)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ArticleItem from './views/components/ArticleItem.vue'
 import Vant from 'vant'
 import 'vant/lib/index.css'
+
+Vue.component('ArticleItem', ArticleItem)
 Vue.use(Vant)
 Vue.config.productionTip = false
 
